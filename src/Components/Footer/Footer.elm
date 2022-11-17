87078c115917e348a3621415f76bcd2cf648d30f
Dmitry Pechenin <pecheneg@student.su>
2022-11-17 20:07:52 +0300

    feat: README and footer update

--- src/Components/Footer/Footer.elm
+++ src/Components/Footer/Footer.elm
@@ -6,9 +6,9 @@ import Html.Attributes exposing (class)
 
 appFooter : Html msg
 appFooter =
-    footer [ class "w-full fixed bottom-0 z-50 bg-white shadow-[0_35px_60px_-15px_rgba(0,0,0,0.5)] py-2" ]
-        [ div [ class " max-w-5xl mx-auto  flex items-center justify-between space-x-4 h-8 px-2" ]
+    footer [ class "footer" ]
+        [ div [ class "footer__content" ]
             [ p [ class "text-m" ] [ text "©2022-2022" ]
-            , p [] [ text " Pokémon and Pokémon character names are trademarks of Nintendo." ]
+            , p [class "text-center text-black/75 mt-4 md:mt-0"] [ text " Pokémon and Pokémon character names are trademarks of Nintendo." ]
             ]
         ]
